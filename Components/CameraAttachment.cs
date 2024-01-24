708535e1775b564a42797fbdb6ab44b138a6c265
williamstrandp <strandenpaurell@outlook.com>
2024-01-24 16:43:28 +0100

    Update CameraAttachment.cs
    Removed unnecessary gameobject

--- Components/CameraAttachment.cs
+++ Components/CameraAttachment.cs
@@ -4,24 +4,8 @@ namespace Components
 {
     public sealed class CameraAttachment
     {
-        public Vector3 Position
-        {
-            get => transform.position;
-            set => transform.position = value;
-        }
+        public Vector3 Position { get; set; }
 
-        public Vector3 Forward
-        {
-            get => transform.forward;
-            set => transform.forward = value;
-        }
-
-        Transform transform;
-
-        public CameraAttachment()
-        {
-            transform = new GameObject("Camera Attachment").transform;
-            transform.hideFlags = HideFlags.HideInHierarchy;
-        }
+        public Vector3 Forward { get; set; }
     }
 }
